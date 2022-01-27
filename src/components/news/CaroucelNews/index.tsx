5470b291dd3ba08e07abc2245505e30c981e1faf
josecontifrwk <joseconti@frwk.com.br>
2022-01-27 17:11:28 -0300

    remove unused clean function

--- src/components/news/CaroucelNews/index.tsx
+++ src/components/news/CaroucelNews/index.tsx
@@ -11,9 +11,6 @@ export default function CaroucelNews() {
     }
     useEffect(() => {
         loadNews();
-
-        return () => {
-        };
     }, []);
 
     return <div className="rounded-t-lg p-1 ">
