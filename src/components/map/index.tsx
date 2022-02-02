482d7e4112f2ba724f100b071f52b5d60c1fd264
josecontifrwk <joseconti@frwk.com.br>
2022-02-02 17:27:43 -0300

    pegar localização + modal

--- src/components/map/index.tsx
+++ src/components/map/index.tsx
@@ -14,18 +14,41 @@ import {
   RouteGeometry,
   RoutingResponseModel,
 } from "../../interfaces/mapboxApiInterfaces";
-import { getDirections, getPlaces } from "../../services/mapbox/mapboxService";
 import { mockedplaces } from "./mockPlaces";
 import { mockedRoute } from "./mockRoute";
 import hM from "../../assets/hospitalMarker.png";
 import uLM from "../../assets/userLocationMarker.svg";
 import MapPopup from "./mapPopup";
+import Modal from "./modal";
 
 const FrwkMap = () => {
   const openStreetApi = "https://a.tile.openstreetmap.org/{z}/{x}/{y}.png";
   const [places, setPlaces] = useState<Array<PlaceModel>>([]);
   const [route, setRoute] = useState<RoutingResponseModel>({});
+  const { lat, lon } = JSON.parse(localStorage.getItem('coordenada'))
+  const [showModal, setShowModal] = useState(false)
+  const [latitude, setLatitude] = useState(lat)
+  const [longitude, setLongitude] = useState(lon)
 
+  function handleShowModal() {
+    setShowModal(!showModal)
+  }
+  function handleAddressCoordinates(lat: number, lon: number) {
+    setLatitude(lat)
+    setLongitude(lon)
+  }
+  async function initiateLocalization() {
+    try {
+      if (lat === 0) {
+        setShowModal(true)
+      }
+    } catch (err) {
+      console.error(err.message)
+    }
+  }
+  useEffect(() => {
+    initiateLocalization()
+  }, []);
   const hospitalMarker = L.icon({
     iconUrl: hM,
     iconSize: [40, 40], // size of the icon
@@ -107,99 +130,133 @@ const FrwkMap = () => {
   }
 
   return (
-    <div style={{ width: "100%", height: "calc(100vh - 82px)" }}>
-      <MapContainer
-        center={[mapCenter[0], mapCenter[1]]}
-        zoom={15}
-        style={{ width: "100%", height: "100%" }}
-      >
-        <TileLayer
-          url={openStreetApi}
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        />
-        {places.map((place) => {
-          const [lng, lat] = place.center;
-          return (
-            <Marker icon={hospitalMarker} key={place.id} position={[lat, lng]}>
-              <MapPopup name={place.text} address={place.place_name} />
-              <Tooltip direction="auto" offset={[0, 0]} opacity={1}>
-                {place.text}
+    <>
+      {showModal ? <div><Modal isOpen={showModal} showModal={handleShowModal} setCoordinates={handleAddressCoordinates} /><img src="https://54337.cdn.simplo7.net/static/54337/sku/digital-digital-exclusivas-tricoline-estampa-digital-mapa-mundo-ref-st-287-1589240857424.jpg" /></div>
+        :
+        <div style={{ width: "100%", height: "calc(100vh - 82px)" }}>
+          <MapContainer
+            center={[latitude, longitude]}
+            zoom={15}
+            style={{ width: "100%", height: "100%" }}
+          >
+            <TileLayer
+              url={openStreetApi}
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            />
+            {places.map((place) => {
+              const [lng, lat] = place.center;
+              return (
+                <Marker icon={hospitalMarker} key={place.id} position={[lat, lng]}>
+                  <MapPopup name={place.text} address={place.place_name} />
+                  <Tooltip direction="auto" offset={[0, 0]} opacity={1}>
+                    {place.text}
+                  </Tooltip>
+                </Marker>
+              );
+            })}
+            <Marker
+              icon={userLocationMarker}
+              position={[latitude, longitude]}
+              key={"root"}
+            >
+              <Tooltip
+                position={[latitude, longitude]}
+                direction="bottom"
+                offset={[0, 0]}
+                opacity={1}
+              >
+                Você está aqui
               </Tooltip>
             </Marker>
-          );
-        })}
-        <Marker
-          icon={userLocationMarker}
-          position={[mapCenter[0], mapCenter[1]]}
-          key={"root"}
-        >
-          <Tooltip
-            position={[mapCenter[0], mapCenter[1]]}
-            direction="bottom"
-            offset={[0, 0]}
-            opacity={1}
-          >
-            Você está aqui
-          </Tooltip>
-        </Marker>
 
-        {route.routes
-          ? getLatLonRouteAndRender(mockedRoute.routes[0].geometry)
-          : ""}
-      </MapContainer>
-    </div>
-import React, { useState, useCallback, useEffect } from "react";
-  import * as ReactDOM from "react-dom";
-  import { MapContainer, TileLayer, Marker, Popup, Pane } from "react-leaflet";
-  import singleSpaReact from "single-spa-react";
-  import { IPlaceModel, mockedplaces } from "../../interfaces/placesInterface";
-  import Modal from "./modal";
-  const FrwkMap = () => {
-    const { lat, lon } = JSON.parse(localStorage.getItem('coordenada'))
-    const [showModal, setShowModal] = useState(false)
-    const [latitude, setLatitude] = useState(lat)
-    const [longitude, setLongitude] = useState(lon)
+            {route.routes
+              ? getLatLonRouteAndRender(mockedRoute.routes[0].geometry)
+              : ""}
+          </MapContainer>
+        </div>}
+    </>
+  );
+};
 
-    async function initiateLocalization() {
-      try {
-        if (lat === 0) {
-          setShowModal(true)
-        }
-      } catch (err) {
-        console.error(err.message)
-      }
-    }
-    useEffect(() => {
-      initiateLocalization()
-    }, []);
+// import React, { useState, useCallback, useEffect } from "react";
+//   import * as ReactDOM from "react-dom";
+//   import { MapContainer, TileLayer, Marker, Popup, Pane } from "react-leaflet";
+//   import singleSpaReact from "single-spa-react";
+//   import { IPlaceModel, mockedplaces } from "../../interfaces/placesInterface";
+//   import Modal from "./modal";
+//   const FrwkMap = () => {
+//     const { lat, lon } = JSON.parse(localStorage.getItem('coordenada'))
+//     const [showModal, setShowModal] = useState(false)
+//     const [latitude, setLatitude] = useState(lat)
+//     const [longitude, setLongitude] = useState(lon)
 
-    return (
-      <>
-        {showModal ? <Modal /> :
-          <div style={{ width: "100%", height: "calc(100vh - 82px)" }}>
+//     async function initiateLocalization() {
+//       try {
+//         if (lat === 0) {
+//           setShowModal(true)
+//         }
+//       } catch (err) {
+//         console.error(err.message)
+//       }
+//     }
+//     useEffect(() => {
+//       initiateLocalization()
+//     }, []);
+//     <>
+//       {showModal ? <Modal isOpen={showModal} showModal={handleShowModal} setCoordinates={handleAddressCoordinates} /> :
+//         <div style={{ width: "100%", height: "calc(100vh - 82px)" }}>
+//           <MapContainer
+//             center={[latitude, longitude]}
+//             zoom={15}
+//             style={{ width: "100%", height: "100%" }}
+//           >
+//             <TileLayer url={openStreetApi} id="mapbox/streets-v11" />
+//             {places.map((place) => {
+//               const [lng, lat] = place.center;
+//               return (
+//                 <Marker key={place.id} position={[lat, lng]}>
+//                   <Popup>{place.place_name}</Popup>
+//                 </Marker>
+//               );
+//             })}
+//             <Marker position={[latitude, longitude]} key={"root"}>
+//               <Popup>Você está aqui</Popup>
+//             </Marker>
 
-            <MapContainer
-              center={[latitude, longitude]}
-              zoom={15}
-              style={{ width: "100%", height: "100%" }}
-            >
+//             {route.routes ? getLatLonRouteAndRender(mockedRoute.routes[0].geometry) : ""}
+//           </MapContainer>
+//         </div>}
+//     </>
+// const FrwkMap = () => {
 
 
-              <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-              <Marker position={[latitude, longitude]}>
-                <Popup>
+//   return (
+//     <>
+//       {showModal ? <Modal /> :
+//         <div style={{ width: "100%", height: "calc(100vh - 82px)" }}>
 
-                </Popup>
-              </Marker>
+//           <MapContainer
+//             center={[latitude, longitude]}
+//             zoom={15}
+//             style={{ width: "100%", height: "100%" }}
+//           >
 
-            </MapContainer>
 
-          </div >}
+//             <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+//             <Marker position={[latitude, longitude]}>
+//               <Popup>
 
-      </>
+//               </Popup>
+//             </Marker>
 
-    );
-  };
+//           </MapContainer>
 
-  export default FrwkMap;
+//         </div >}
+
+//     </>
+
+//   );
+// };
+
+export default FrwkMap;
 
--- src/components/map/modal/index.tsx
+++ src/components/map/modal/index.tsx
@@ -1,23 +1,53 @@
-/* This example requires Tailwind CSS v2.0+ */
-import { Fragment, useRef, useState } from 'react'
+import { Fragment, useEffect, useRef, useState } from 'react'
 import { Dialog, Transition } from '@headlessui/react'
 import { MapIcon } from '@heroicons/react/outline'
 import { InputAddress } from './styles'
+import { getAddress } from '../../../services/mapbox/mapboxService'
+import { IFeature } from '../../../interfaces/placesInterface'
 
-export default function Modal() {
-    const [open, setOpen] = useState(true)
+interface IModal {
+    isOpen: boolean
+    showModal: () => void
+    coordenadas?: [number, number]
+    setCoordinates: (lat: number, lon: number) => void
+}
+export default function Modal({ showModal, isOpen, setCoordinates }: IModal) {
+    const [address, setAddress] = useState('')
+    const [addressList, setAddressList] = useState(null)
+    useEffect(() => {
+        if (address.length >= 3) {
+            handleAddressChange(address)
+        } else {
+            setAddressList([])
+        }
+    }, [address]);
+    async function handleAddressChange(addressInput: string) {
+        const { features } = await getAddress(addressInput)
+        setAddressList(features)
 
-    const cancelButtonRef = useRef(null)
+    }
+    function handleSetAddress({ currentTarget }: { currentTarget: Element }) {
+        try {
+            setAddressList([])
+            setAddress(currentTarget.id)
+            const [lat, lon] = currentTarget.children[2].textContent.split(",")
+            setCoordinates(parseFloat(lat), parseFloat(lon))
+            showModal()
+        } catch (error) {
+            console.log(error.message)
+        }
 
+    }
+    const addressRef = useRef(null)
     return (
-        <Transition.Root show={open} as={Fragment}>
-            <Dialog as="div" className="fixed z-10 inset-0 overflow-y-auto" initialFocus={cancelButtonRef} onClose={setOpen}>
+        <Transition.Root show={isOpen} as={Fragment}>
+            <Dialog as="div" className="fixed z-10 inset-0 overflow-y-auto" initialFocus={addressRef} onClose={showModal}>
                 <div className="flex items-end  min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
                     <Transition.Child
                         as={Fragment}
                         enter="ease-out duration-300"
                         enterFrom="opacity-0"
-                        enterTo="opacity-100"
+                        enterTo="opacity-200"
                         leave="ease-in duration-200"
                         leaveFrom="opacity-100"
                         leaveTo="opacity-0"
@@ -38,36 +68,52 @@ export default function Modal() {
                         leaveFrom="opacity-100 translate-y-0 sm:scale-100"
                         leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
                     >
-                        <div className="inline-block  align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all lg:my-8 lg:align-middle max-w-xl lg:w-full ">
-                            <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4 w-full">
+                        <div className="inline-block align-bottom  rounded-lg text-left overflow-hidden shadow-xl transform transition-all lg:my-8 lg:align-middle max-w-xl lg:w-full ">
+                            <div className="bg-cyan-500 px-4 pt-5 pb-4 sm:p-6 sm:pb-4 w-full">
                                 <div className="sm:flex sm:items-start">
                                     <div className="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-cyan-100 sm:mx-0 sm:h-10 sm:w-10">
                                         <MapIcon className="h-6 w-6 text-cyan-600" aria-hidden="true" />
                                     </div>
                                     <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left w-full">
-                                        <Dialog.Title as="h3" className="text-lg leading-6 font-medium text-gray-900">
-                                            Informe a sua localização
+                                        <Dialog.Title as="h3" className="text-lg leading-6 font-medium text-white">
+                                            Qual é a sua localização?
                                         </Dialog.Title>
-                                        <div className="mt-2 w-full">
-                                            <InputAddress />
+                                        <div className="mt-2 w-full flex flex-col h-auto text-white">
+                                            <div className='flex flex-row'><InputAddress onChange={(e) => setAddress(e.target.value)} value={address} placeholder='Ex: Rua, Número, Bairro, Cidade - Estado' />
+                                                {/* <button
+                                                    type="button"
+                                                    className="w-full border-white border-2 rounded-full shadow-sm px-4 py-2 bg-transparent text-base font-medium text-white hover:bg-cyan-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-500 sm:ml-3 sm:w-auto sm:text-sm"
+                                                    onClick={showModal}
+                                                >
+                                                    &gt;
+                                                </button> */}
+                                            </div>
+                                            {addressList ? <ul>
+                                                {addressList.map((addressItem: IFeature) => {
+                                                    const [title, subTitle, lastTitle = ''] = addressItem.place_name.split(",")
+                                                    const [lng, lat] = addressItem.geometry.coordinates
+                                                    return <li onClick={handleSetAddress} id={addressItem.place_name} className='cursor-pointer space-y-1 p-2 flex flex-col rounded hover:bg-cyan-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-400   ' key={addressItem.id}>
+                                                        <h4 className='font-semibold text-lg'>{title}</h4>
+                                                        <h5 className='text-sm'>{`${subTitle},${lastTitle}`}</h5>
+                                                        <p className='hidden'>{`${lat},${lng}`}</p>
+                                                    </li>
+                                                })}
+                                            </ul> : ''}
+
                                         </div>
+
                                     </div>
                                 </div>
                             </div>
-                            <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
-                                <button
-                                    type="button"
-                                    className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-cyan-600 text-base font-medium text-white hover:bg-cyan-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-500 sm:ml-3 sm:w-auto sm:text-sm"
-                                    onClick={() => setOpen(false)}
-                                >
-                                    Continuar
-                                </button>
 
-                            </div>
+
                         </div>
                     </Transition.Child>
+
                 </div>
-            </Dialog>
-        </Transition.Root>
+
+            </Dialog >
+
+        </Transition.Root >
     )
 }
