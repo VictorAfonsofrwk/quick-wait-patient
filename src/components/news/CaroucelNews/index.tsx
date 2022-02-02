4923e00c11a0165fd746270172fc93604b67c7c5
raiffe.pontes <raiffepontes@frwk.com.br>
2022-02-02 15:51:04 -0300

    Adjustes no carousel

--- src/components/news/CaroucelNews/index.tsx
+++ src/components/news/CaroucelNews/index.tsx
@@ -1,24 +1,30 @@
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css"
 import { IArticles } from "../../../interfaces/newsInterface";
+import { Container, ImageNews } from './styles';
 
 interface IListArticles {
   articles: IArticles[];
 }
 
 export default function CaroucelNews({ articles }: IListArticles) {
-  return <div className="rounded-t-lg p-1 ">
-    <Carousel 
-      showArrows 
-      autoPlay 
-      dynamicHeight 
-      renderThumbs={(children: React.ReactChild[]) => []}>
-      {articles.map((article: IArticles, index) => {
-        return <div key={index}>
-          <img className="h-96" src={article.urlToImage} />
-          <p className="legend">{article.title}</p>
-        </div>
-      })}
-    </Carousel>
-  </div >
+  return (
+    <Container>
+      <Carousel
+        showArrows
+        autoPlay
+        dynamicHeight
+        renderThumbs={(children: React.ReactChild[]) => []}>
+        {articles.map((article: IArticles, index) => {
+          return (
+            <div key={index}>
+              <ImageNews 
+                style={{ backgroundImage: `url(${article.urlToImage})` }} />
+              <p className="legend">{article.title}</p>
+            </div>
+          )
+        })}
+      </Carousel>
+    </Container>
+  )
 }
