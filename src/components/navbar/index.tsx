f316c78f1c9e6c4f51f75bce8f0553dee27d660d
josecontifrwk <joseconti@frwk.com.br>
2022-02-16 10:05:58 -0300

    about caroucel working

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -3,6 +3,7 @@ import Subtract from "../../assets/Subtract.svg";
 import map from "../../assets/map-svgrepo-com.svg";
 import news from "../../assets/newspaper-svgrepo-com.svg";
 import stylebar from "../../assets/stylebar.svg";
+import light from '../../assets/light.svg'
 import {
   NavBarMenu,
   NavContainer,
@@ -56,6 +57,7 @@ export default function NavBar() {
             </LinkToHome>
           </LogoHome>
           <NavBarListItem>
+
             <NavItem id="hospitais">
               <ActiveRoute to="/">
                 <img src={map} alt="hospitais" />
@@ -64,8 +66,14 @@ export default function NavBar() {
             </NavItem>
             <NavItem id="noticias">
               <ActiveRoute to="/noticias">
-                <img src={news} alt="hospitais" className="text-cyan-600" />
+                <img src={news} alt="noticias" className="text-cyan-600" />
                 <span className="ml-2">Noticias</span>
+              </ActiveRoute>
+            </NavItem>
+            <NavItem id="sobre">
+              <ActiveRoute to="/sobre">
+                <img src={light} alt="sobre" className="text-cyan-600" />
+                <span className="ml-2">Sobre</span>
               </ActiveRoute>
             </NavItem>
             <li className="nav-item h-full -m-1">
@@ -79,6 +87,7 @@ export default function NavBar() {
                   className="text-sm rounded-lg shadow-md py-2 px-6 font-semibold bg-white text-gray-500 ">Entrar</button></Link>}
 
             </li>
+
           </NavBarListItem>
         </NavContainer>
       </NavBarMenu>
