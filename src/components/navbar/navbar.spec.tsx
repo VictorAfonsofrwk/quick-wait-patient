8207b0d72f541f265c031a991ae6a765113d67da
AdrianoValadaresFrwk <adrianovaladares@frwk.com.br>
2022-03-09 09:20:57 -0300

    Update navbar.spec.tsx

--- src/components/navbar/navbar.spec.tsx
+++ src/components/navbar/navbar.spec.tsx
@@ -1,2 +1,18 @@
 import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import { mocked } from "jest-mock";
+import Navbar from "./index"
+
+
+describe('HeaderComponent', () => { 
+
+  it('Should have 3 options', async () => {
+    render(<Navbar />);
+    await waitFor(() => {
+      expect(screen.getAllByRole('link')).toHaveLength(3);
+    });
+  });
+
+
+
+
+ })
