951b8c6419ecfcc967e7f32a6f826b0cc3cf52b3
AdrianoValadaresFrwk <adrianovaladares@frwk.com.br>
2022-03-09 10:08:31 -0300

    teste de deploy usando procfile do heroku

--- webpack.config.js
+++ webpack.config.js
@@ -32,5 +32,6 @@ module.exports = (webpackConfigEnv, argv) => {
 
   return merge(defaultConfig, {
     // modify the webpack config however you'd like to by adding to this object
+    mode: "production",
   });
 };
